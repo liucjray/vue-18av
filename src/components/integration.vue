a6d2226828d3d6cd769c919f724b18f04d909aea
cj <cj@gmail.com>
2021-12-15 13:10:53 +0800

    integration - feature pagination done

--- src/components/integration.vue
+++ src/components/integration.vue
@@ -13,11 +13,21 @@
         :label="category.shortname"
         :name="category.CHID"
       >
+        <el-pagination
+          style="width: 50%"
+          background
+          layout="prev, pager, next"
+          :total="videosTotal"
+          @current-change="vidoePageClick"
+          @prev-click="videosPrevClick"
+          @next-click="videosNextClick"
+        >
+        </el-pagination>
         <el-table
           style="width: 50%"
           :data="videos"
-          :loading="isLoading"
-          element-loading-text="拼命加载中"
+          v-loading="isLoading"
+          element-loading-text="載入中"
           element-loading-spinner="el-icon-loading"
           element-loading-background="rgba(0, 0, 0, 0.8)"
         >
@@ -33,7 +43,13 @@
             label="片名"
             width="500px"
           ></el-table-column>
-          <el-table-column prop="duration" label="時長"> </el-table-column>
+          <el-table-column prop="duration" label="時長">
+            <template slot-scope="scope">
+              <el-button icon="el-icon-timer" effect="dark">
+                {{ scope.row.duration }}
+              </el-button>
+            </template>
+          </el-table-column>
         </el-table>
       </el-tab-pane>
     </el-tabs>
@@ -47,10 +63,16 @@ export default {
   name: "categories",
   data() {
     return {
-      activeCHID: "1",
+      // 是否讀取
       isLoading: false,
+      // 影片分類
+      activeCHID: "1",
       categories: [],
+      // 影片
       videos: [],
+      videosLimit: 8,
+      videosTotal: 0,
+      videosCurrentPage: 0,
     };
   },
   beforeMount() {
@@ -61,30 +83,52 @@ export default {
   },
   methods: {
     switchCategory() {
-      this.getVideos(this.activeCHID);
+      this.videosCurrentPage = 0;
+      this.getVideos();
     },
     getCategories() {
+      this.isLoading = true;
       axios
         .get("https://api.avgle.com/v1/categories")
         .then(({ data }) => {
-          this.isLoading = true;
+          this.isLoading = data.success != true;
           this.categories = data.response.categories;
         })
         .catch((error) => {
           console.error(error);
         });
     },
-    getVideos(CHID) {
+    getVideos() {
       this.isLoading = true;
+      let url = `https://api.avgle.com/v1/videos/${this.videosCurrentPage}?c=${this.activeCHID}&limit=${this.videosLimit}`;
+      console.log(url);
       axios
-        .get(`https://api.avgle.com/v1/videos/0?c=${CHID}&limit=8`)
+        .get(url)
         .then(({ data }) => {
-          this.isLoading = data.response.success != true;
+          this.isLoading = data.success != true;
+          this.videosTotal = data.response.total_videos;
           this.videos = data.response.videos;
         })
         .catch((error) => {
           console.error(error);
         });
+    },
+    videosPrevClick() {
+      this.videosCurrentPage -= 1;
+      this.getVideos();
+    },
+    videosNextClick() {
+      this.videosCurrentPage += 1;
+      this.getVideos();
+    },
+    vidoePageClick(page) {
+      this.videosCurrentPage = page - 1;
+      this.getVideos();
+    },
+  },
+  computed: {
+    transformHumanTIme(seconds) {
+      return seconds;
     },
   },
 };
